4f56d9d5804de360543f8d5a9f596fb996ea8ba7
Arteezy09 <arteezybezskilla@mail.ru>
2016-12-04 19:21:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,12 +4,12 @@
 #include <iostream>
 #include <fstream>
 
-/* SCENARIO("8mb", "[8mb]")
+ SCENARIO("8mb", "[8mb]")
  {
   REQUIRE(1 == 1);
- }*/
+ }
 
-SCENARIO("8mb", "[8mb]")
+/*SCENARIO("8mb", "[8mb]")
 {
   _sort other("8mb", "out_8", 1);
   bool x = true;
@@ -30,19 +30,19 @@ SCENARIO("8mb", "[8mb]")
   f11.close();
   f22.close();
   REQUIRE(x);
-}
+}*/
 
 // SCENARIO("15mb", "[15mb]")
 // {
-//   File_sort other("15mb", "out_15", 4);
+//   _sort other("15mb", "out_15", 4);
 //   bool x = true;
-//   std::ifstream f1("out_15"), f2("out15");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
+//   std::ifstream f11("out_15"), f22("out15");
+//   stud st1, st2;
+//   while(!f22.eof() && !f11.eof())
 //   {
-//     if((f1>>s1)&&(f2>>s2))
+//     if((f11>>st1)&&(f22>>st2))
 //     {
-//       if(s1.name!=s2.name)
+//       if(st1.name!=st2.name)
 //       {
 //         x=false;
 //         break;
@@ -50,22 +50,22 @@ SCENARIO("8mb", "[8mb]")
 //     }
 //     else break;
 //   }
-//   f1.close();
-//   f2.close();
+//   f11.close();
+//   f22.close();
 //   REQUIRE(x);
 // }
 
 // SCENARIO("32mb", "[32mb]")
 // {
-//   File_sort other("32mb", "out_32", 17);
+//   _sort other("32mb", "out_32", 17);
 //   bool x = true;
-//   std::ifstream f1("out_32"), f2("out32");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
+//   std::ifstream f11("out_32"), f22("out32");
+//   stud st1, st2;
+//   while(!f22.eof() && !f11.eof())
 //   {
-//     if((f1>>s1)&&(f2>>s2))
+//     if((f11>>st1)&&(f22>>st2))
 //     {
-//       if(s1.name!=s2.name)
+//       if(st1.name!=st2.name)
 //       {
 //         x=false;
 //         break;
@@ -73,7 +73,7 @@ SCENARIO("8mb", "[8mb]")
 //     }
 //     else break;
 //   }
-//   f1.close();
-//   f2.close();
+//   f11.close();
+//   f22.close();
 //   REQUIRE(x);
 // }
