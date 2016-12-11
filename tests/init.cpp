30dc2296ab207aef8d60545828d27ae87ca72751
Arteezy09 <arteezybezskilla@mail.ru>
2016-12-11 18:28:59 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -27,48 +27,48 @@ SCENARIO("8mb", "[8mb]")
   REQUIRE(x);
 }
 
-// SCENARIO("15mb", "[15mb]")
-// {
-//   File_sort other("15mb", "out_15", 4);
-//   bool x = true;
-//   std::ifstream f1("out_15"), f2("out15");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
-//   {
-//     if((f1>>s1)&&(f2>>s2))
-//     {
-//       if(s1.name!=s2.name)
-//       {
-//         x=false;
-//         break;
-//       }
-//     }
-//     else break;
-//   }
-//   f1.close();
-//   f2.close();
-//   REQUIRE(x);
-// }
+SCENARIO("15mb", "[15mb]")
+{
+  Sort other("15mb", "out_15", 4);
+  bool x = true;
+  std::ifstream f1("out_15"), f2("out15");
+  S st1, st2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>st1)&&(f2>>st2))
+    {
+      if(st1.name!=st2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
 
-// SCENARIO("32mb", "[32mb]")
-// {
-//   File_sort other("32mb", "out_32", 17);
-//   bool x = true;
-//   std::ifstream f1("out_32"), f2("out32");
-//   Data s1, s2;
-//   while(!f2.eof() && !f1.eof())
-//   {
-//     if((f1>>s1)&&(f2>>s2))
-//     {
-//       if(s1.name!=s2.name)
-//       {
-//         x=false;
-//         break;
-//       }
-//     }
-//     else break;
-//   }
-//   f1.close();
-//   f2.close();
-//   REQUIRE(x);
-// }
+SCENARIO("32mb", "[32mb]")
+{
+  Sort other("32mb", "out_32", 17);
+  bool x = true;
+  std::ifstream f1("out_32"), f2("out32");
+  S st1, st2;
+  while(!f2.eof() && !f1.eof())
+  {
+    if((f1>>st1)&&(f2>>st2))
+    {
+      if(st1.name!=st2.name)
+      {
+        x=false;
+        break;
+      }
+    }
+    else break;
+  }
+  f1.close();
+  f2.close();
+  REQUIRE(x);
+}
